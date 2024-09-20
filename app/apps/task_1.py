e04c5d185f125f7d706ab92a80825489e0d026f5
nebiyu-ethio <nebiyuethio@gmail.com>
2024-09-20 13:42:56 +0300

    update

--- app/apps/task_1.py
+++ app/apps/task_1.py
@@ -9,8 +9,8 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 from scripts.data_processing import *
 
 def app():
-    # Load Data
-    zip_file_path = os.path.abspath('MachineLearningRating_v3.zip')
+    # Load Data 
+    zip_file_path = os.path.join(os.path.dirname(__file__), 'MachineLearningRating_v3.zip')
     csv_file_name =  os.path.abspath('MachineLearningRating_v3.txt')  # Name of the file inside the zip
 
     with zipfile.ZipFile(zip_file_path, 'r') as z:
